e682db94d2c1e756df8a777016390c777af65239
hemangjuneja25 <hemangjuneja25@gmail.com>
2021-02-24 11:15:33 +0530

    new_commit

--- model/index.js
+++ model/index.js
@@ -1,14 +1,20 @@
 const mongoose = require('mongoose');
 
+
+/*
 mongoose.connect('mongodb+srv://naveen123:Pass123@cluster0-nxrhl.mongodb.net/secondDb?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true }, (err)=>{
 if(!err) {console.log('mongodb selected') }
 }); 
+*/
 
 
-/*
-mongoose.connect('mongodb://instrovate-db:pVNDo50Q649eQrMy9nV2gXAGdg3HITb6ZrNOb1grfEezPrW2Q9UfNJN2VPLPnaZKt6HcnPrHt0beduZhInZryA==@instrovate-db.mongo.cosmos.azure.com:10255/?ssl=true&appName=@instrovate-db@', {useNewUrlParser: true, useUnifiedTopology: true }, (err)=>{
+
+
+mongoose.connect('mongodb+srv://akriti_lal:Pass123@cluster0.wncdg.mongodb.net/instrovate?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true }, (err)=>{
 if(!err) {console.log('mongodb selected') }
-}); 
+});
+
+
 /*
 mongoose.connect('mongodb://localhost:27017/firstDb?readPreference=primary&appname=MongoDB%20Compass%20Community&ssl=false', {useNewUrlParser: true, useUnifiedTopology: true }, (err)=>{
 if(!err) {console.log('mongodb selected') }
